5d85f86679d94ae952573b5d9ffcb12f9eba5a7e
oleg <oleg.kh81@gmail.com>
2018-09-05 16:45:02 +0300

    AddressTests init

--- src/test/java/com/academy/automationpractice/ddt/tests/AddressTests.java
+++ src/test/java/com/academy/automationpractice/ddt/tests/AddressTests.java
@@ -1,4 +1,14 @@
 package com.academy.automationpractice.ddt.tests;
 
-public class AddressTests {
+import org.testng.annotations.Test;
+
+public class AddressTests extends BaseTest {
+
+    @Test
+    // TODO
+    public void testAddAddress() {
+        System.out.println("start 'testAddAddress'");
+
+        System.out.println("complete 'testAddAddress'");
+    }
 }
