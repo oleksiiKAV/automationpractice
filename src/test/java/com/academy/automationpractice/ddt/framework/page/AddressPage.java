d44baafb9ddd08f3c04343c3ddbd947aa47d90f0
oleg <oleg.kh81@gmail.com>
2018-09-07 11:47:21 +0300

    fix: move 'FormAddressPage' to 'page' packet

--- src/test/java/com/academy/automationpractice/ddt/framework/page/AddressPage.java
+++ src/test/java/com/academy/automationpractice/ddt/framework/page/AddressPage.java
@@ -1,6 +1,5 @@
 package com.academy.automationpractice.ddt.framework.page;
 
-import com.academy.automationpractice.ddt.framework.helper.FormAddressPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
