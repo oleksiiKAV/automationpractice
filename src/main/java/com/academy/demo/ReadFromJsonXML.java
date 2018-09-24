076abd474c98790e9a4c38c047ea83953abb95f7
Oleg <oleg.kh81@gmail.com>
2018-09-24 09:35:31 +0300

    jmeter/soap -> add implemented

--- src/main/java/com/academy/demo/ReadFromJsonXML.java
+++ src/main/java/com/academy/demo/ReadFromJsonXML.java
@@ -1,5 +1,8 @@
 package com.academy.demo;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
@@ -9,22 +12,28 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFromJsonXML {
     public static void main(String[] args) {
         String path = System.getProperty("user.dir") + "/data/login";
-        System.out.println("Read from JSON: ");
+
         readFromJson(path+".json")
                 .forEach(System.out::println);
 
-        System.out.println("Read from XML: ");
         readFromXML(path+".xml")
                 .forEach(System.out::println);
+
+        readUsingMinimalJsonLib(path+".json")
+                .forEach(System.out::println);
+
     }
 
     // Читаем из XML
     private static List<LoginData> readFromXML(String path) {
+        System.out.println(String.format("Read xml from %s: ", path));
+
         String xml = readFileAsString(path);
         XStream xStream = new XStream();
         initXstreamSecurity(xStream);
@@ -35,11 +44,31 @@ public class ReadFromJsonXML {
 
     // Читаем из JSON
     private static List<LoginData> readFromJson(String path) {
+        System.out.println(String.format("Read json from %s: ", path));
 
         String json = readFileAsString(path);
         Type listType = new TypeToken<List<LoginData>>() {}.getType();
         List<LoginData> data = new Gson().fromJson(json, listType);
         return data;
+    }
+
+    private static List<LoginData> readUsingMinimalJsonLib(String path) {
+        System.out.println(String.format("Read using minimal-json lib from %s: ", path));
+
+        String json = readFileAsString(path);
+        JsonValue jsonValue = Json.parse(json);
+        List<LoginData> data = new ArrayList<>();
+        jsonValue.asArray().iterator().forEachRemaining(v-> data.add(parseJsonValueToLoginData(v)));
+
+        return data;
+    }
+
+    private static LoginData parseJsonValueToLoginData(JsonValue jsonValue) {
+        JsonObject jsonObject = jsonValue.asObject();
+        return new LoginData(
+                jsonObject.get("login").toString(),
+                jsonObject.get("password").toString(),
+                jsonObject.get("msg").toString());
     }
 
     // Читаем из файла в строку как есть
@@ -64,3 +93,4 @@ public class ReadFromJsonXML {
         xStream.allowTypes(classes);
     }
 }
+
