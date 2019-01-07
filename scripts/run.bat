c6aefcf41119e596b21d9c4624b78a79cda9c4e6
Oleg <oleg.kh81@gmail.com>
2019-01-07 23:56:28 +0200

    fix testng - report dir

--- scripts/run.bat
+++ scripts/run.bat
@@ -8,10 +8,11 @@ set log=../cfg/log4j2.properties
 set main=org.testng.TestNG
 set suiteDefault=../suits/testng.xml
 set suiteParam=%1
+set report.dir=../report
 if defined suiteParam (
 	set suite=%suiteParam%
 ) else (
 	set suite=%suiteDefault%
 )
 
-java -cp %cp% -Dautomation.cfg=%automation.prop% -Dmobile.cfg=%mobile.prop% -Dcommon.cfg=%common.prop% -DLogj4=%log% %main%  %suite%
+java -cp %cp% -Dautomation.cfg=%automation.prop% -Dmobile.cfg=%mobile.prop% -Dcommon.cfg=%common.prop% -DLogj4=%log% %main%  %suite% -d %report.dir%
